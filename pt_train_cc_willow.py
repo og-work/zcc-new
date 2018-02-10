ecb178228ec56f9db9b66fc2c5ca0e3eb11a5323
og-work <og.work2786@gmail.com>
2018-02-10 16:15:52 +0530

    Add files via upload
    Model working on GPU now but running very slowly. Accuracy is improving initially but saturating to 40 to 45%.
    -added train and test functionality
    -saving and loading model
    -displaying accuracy

--- pt_train_cc_willow.py
+++ pt_train_cc_willow.py
@@ -10,7 +10,9 @@
 TODO List:
 -add L2 regularizer loss for weights
 -biases
-
+-adding noise to sample
+-load and store and resume training
+-test/evaluation
 
 '''
 
@@ -26,19 +28,22 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 from pt_get_data_willow import classifier_data, input_cc
+import torch.nn.functional as F
 
 num_epochs = 50
-batch_size = 64
-learning_rate = 1e-3
+learning_rate = 0.0001
 alpha = 0.5
 NUMBER_OF_CLASSES = 7
 MIN_NUMBER_OF_SAMPLES_OF_CLASS = 70
 TR_TS_VA_SPLIT = [0.7, 0.3]
 NUMBER_OF_TRAIN_SAMPLES_IN_SUBSET = int(TR_TS_VA_SPLIT[0] * MIN_NUMBER_OF_SAMPLES_OF_CLASS)
 NUMBER_OF_PERMUTED_SAMPLES = NUMBER_OF_TRAIN_SAMPLES_IN_SUBSET * NUMBER_OF_TRAIN_SAMPLES_IN_SUBSET
-BATCH_SIZE = 4
-NUMBER_OF_BATCHES = int(NUMBER_OF_PERMUTED_SAMPLES / BATCH_SIZE)
-
+TRAIN_BATCH_SIZE = 64
+NUMBER_OF_BATCHES = int(NUMBER_OF_PERMUTED_SAMPLES / TRAIN_BATCH_SIZE)
+NUMBER_OF_TEST_SAMPLES_ALL_CLASSES = 210
+TEST_BATCH_SIZE = 64
+NUMBER_OF_TEST_BATCHES = int(NUMBER_OF_TEST_SAMPLES_ALL_CLASSES / TEST_BATCH_SIZE)
+MODEL_SAVE_PATH = './e2e_model_1.pth'
 
 class train_pt_cc_input:
         cc1_input_train_perm = np.array([])
@@ -96,6 +101,26 @@ def precompute_training_indices(labels, train_classes):
 	return indices_input_class_samples_train, indices_output_class_samples_train
 
 
+def get_testing_data(batch, data, labels, test_classes):
+	input_data = np.empty((0, data.shape[1]), float)
+	batch_labels = []	
+	m = 0
+	test_indices = []
+	for this_class in test_classes:
+		indices_class_samples = np.flatnonzero(labels == this_class)
+		test_indices.append(indices_class_samples)
+	test_indices = np.concatenate(test_indices).ravel()
+	input_start_ind = batch * TEST_BATCH_SIZE 
+	input_end_ind = input_start_ind + TEST_BATCH_SIZE
+	input_batch_indices = test_indices[input_start_ind:input_end_ind]
+	input_batch_labels = labels[input_batch_indices]
+	input_data = data[input_batch_indices, :]
+	input_batch_labels = np.asarray(input_batch_labels)
+	input_batch_labels = input_batch_labels.flatten()
+	
+	return input_data, input_batch_labels
+
+
 def get_training_data(batch, data, labels, train_classes):
 	input_data = np.empty((0, data.shape[1]), float)
 	output_data = np.empty((0, data.shape[1]), float)
@@ -108,17 +133,17 @@ def get_training_data(batch, data, labels, train_classes):
 
 	for input_class in train_classes:
 		k = 0
-		input_start_ind = batch * BATCH_SIZE 
-		#input_start_ind = batch * BATCH_SIZE + m * NUMBER_OF_PERMUTED_SAMPLES	
-		input_end_ind = input_start_ind + BATCH_SIZE
+		input_start_ind = batch * TRAIN_BATCH_SIZE 
+		#input_start_ind = batch * TRAIN_BATCH_SIZE + m * NUMBER_OF_PERMUTED_SAMPLES	
+		input_end_ind = input_start_ind + TRAIN_BATCH_SIZE
 		input_class_indices = np.asarray(indices_all_class_samples_train[m])
 		input_class_indices = np.repeat(input_class_indices, np.size(input_class_indices))
 		m = m + 1
 		for output_class in train_classes:
 			#print "Preparing data for class pair (%d >> %d)"%(input_class, output_class)
-			#output_start_ind = batch * BATCH_SIZE + k * NUMBER_OF_PERMUTED_SAMPLES
-			output_start_ind = batch * BATCH_SIZE
-			output_end_ind = output_start_ind + BATCH_SIZE
+			#output_start_ind = batch * TRAIN_BATCH_SIZE + k * NUMBER_OF_PERMUTED_SAMPLES
+			output_start_ind = batch * TRAIN_BATCH_SIZE
+			output_end_ind = output_start_ind + TRAIN_BATCH_SIZE
 			output_class_indices = np.asarray(indices_all_class_samples_train[k])
 			output_class_indices = np.tile(output_class_indices, np.size(output_class_indices))
 			input_batch_indices = input_class_indices[input_start_ind:input_end_ind] 
@@ -126,10 +151,12 @@ def get_training_data(batch, data, labels, train_classes):
 			input_data = np.append(input_data, data[input_batch_indices, :], axis=0)
 			output_data = np.append(output_data, data[output_batch_indices, :], axis=0)
 			k = k + 1
-			batch_labels.append([input_class]*BATCH_SIZE)
+			batch_labels.append([input_class]*TRAIN_BATCH_SIZE)
 	batch_labels = np.asarray(batch_labels)
 	batch_labels = batch_labels.flatten()
-	
+
+	if input_data.shape[0] != batch_labels.shape[0]:
+	    raise AssertionError("Dimension mismatch!")	
 	return input_data, output_data, batch_labels
  
 '''
@@ -144,18 +171,34 @@ class mlp_classifier(nn.Module):
         super(mlp_classifier, self).__init__()
 	self.input_feature_dim = input_feature_dim
 	self.number_of_classes = number_of_classes
-        self.classifier = nn.Sequential(
-            nn.Linear(self.input_feature_dim, int(self.input_feature_dim*0.5)),
-            nn.ReLU(True),
-            nn.Linear(int(self.input_feature_dim*0.5), int(self.input_feature_dim*0.25)),
-            nn.ReLU(True),
-            nn.Linear(int(self.input_feature_dim*0.25), int(self.input_feature_dim*0.1)),
-	    nn.ReLU(True),
-            nn.Linear(int(self.input_feature_dim*0.1), self.number_of_classes))
+        if 0:
+		self.classifier = nn.Sequential(
+		    nn.Linear(self.input_feature_dim, int(self.input_feature_dim*0.5)),
+		    nn.ReLU(True),
+		    nn.Linear(int(self.input_feature_dim*0.5), int(self.input_feature_dim*0.25)),
+		    nn.ReLU(True),
+		    nn.Linear(int(self.input_feature_dim*0.25), int(self.input_feature_dim*0.1)),
+		    nn.ReLU(True),
+		    nn.Linear(int(self.input_feature_dim*0.1), self.number_of_classes))
 
-    def forward(self, data):
-        predicted_labels = self.classifier(data)
-        return predicted_labels
+	else:
+
+		self.fc1 = nn.Linear(self.input_feature_dim, int(self.input_feature_dim*0.5))
+		self.fc2 = nn.Linear(int(self.input_feature_dim*0.5), int(self.input_feature_dim*0.25))
+		self.fc3 = nn.Linear(int(self.input_feature_dim*0.25), int(self.input_feature_dim*0.1))
+		self.fc4 = nn.Linear(int(self.input_feature_dim*0.1), int(self.number_of_classes))
+			
+    def forward(self, x):
+	if 0:
+		predicted_labels = self.classifier(x)
+		return predicted_labels
+	else:
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+		x = self.fc4(x)
+
+		return F.log_softmax(x, dim=1)
 
 '''
 ---------------------------------------------
@@ -188,7 +231,6 @@ E2E Network
 ---------------------------------------------
 ---------------------------------------------
 '''
-
 class E2E_NETWORK_TRAIN(nn.Module):
 	def __init__(self, input_output_dim, hidden_layer_dim, number_of_classes, train_class_labels):
 		super(E2E_NETWORK_TRAIN, self).__init__()
@@ -198,22 +240,27 @@ class E2E_NETWORK_TRAIN(nn.Module):
 		self.train_class_labels = train_class_labels
 		self.input_feature_dim = hidden_layer_dim * number_of_classes * number_of_classes
 		self.mlp_classifier = mlp_classifier(self.input_feature_dim, self.number_of_classes)
-		#self.mlp_classifier.cuda()
+		self.mlp_classifier.cuda()
 		self.ae_bank = []
+		ae_model = autoencoder(self.input_output_dim, self.hidden_layer_dim)
+		pdb.set_trace()
 		for i in range(self.number_of_classes*self.number_of_classes):
-			self.ae_bank.append(autoencoder(self.input_output_dim, self.hidden_layer_dim))
+			self.ae_bank.append(ae_model.cuda())
 
 	def forward(self, data):
 		encoded_feature_conc = np.empty((0, self.hidden_layer_dim), dtype=float)		
 		decoded_feature_conc = np.empty((0, self.input_output_dim), dtype=float)
+		#encoded_feature_conc = get_pytorch_variable(encoded_feature_conc)
+		#decoded_feature_conc = get_pytorch_variable(decoded_feature_conc)
+		
 		#Passing input class data to corresponding cc
 		k = 0		
 		for input_class in self.train_class_labels:
 			for output_class in self.train_class_labels:
-				start_ind = k * BATCH_SIZE
-				end_ind = start_ind + BATCH_SIZE
+				start_ind = k * TRAIN_BATCH_SIZE
+				end_ind = start_ind + TRAIN_BATCH_SIZE
 				input_data_cc = data[start_ind:end_ind, :]
-				encoded_features, decoded_features = self.ae_bank[k](get_pytorch_variable(input_data_cc))
+				encoded_features, decoded_features = self.ae_bank[k]((input_data_cc))
 				#encoded_feature_conc = np.append(encoded_feature_conc, encoded_features.data.cpu().numpy(), axis=0)
 				decoded_feature_conc = np.append(decoded_feature_conc, decoded_features.data.cpu().numpy(), axis=0)
 				k = k + 1
@@ -223,22 +270,60 @@ class E2E_NETWORK_TRAIN(nn.Module):
 		clfr_features = np.empty((0, self.hidden_layer_dim*NUMBER_OF_CLASSES*NUMBER_OF_CLASSES), dtype=float)		
 		for input_class in self.train_class_labels:
 			for output_class in self.train_class_labels:
-				start_ind = k * BATCH_SIZE
-				end_ind = start_ind + BATCH_SIZE
+				start_ind = k * TRAIN_BATCH_SIZE
+				end_ind = start_ind + TRAIN_BATCH_SIZE
 				input_data_cc = data[start_ind:end_ind, :]
 				p = 0
 				clfr_encoded_feature_conc = np.empty((input_data_cc.shape[0], 0), dtype=float)		
 				for classI in self.train_class_labels:
 					for classJ in self.train_class_labels:
-						clfr_encoded_features, clfr_decoded_features = self.ae_bank[p](get_pytorch_variable(input_data_cc))
+						clfr_encoded_features, clfr_decoded_features = self.ae_bank[p](input_data_cc)
 						#Concat hidden features along columns for all cc
 						clfr_encoded_feature_conc = np.append(clfr_encoded_feature_conc, clfr_encoded_features.data.cpu().numpy(), axis=1)
 						p = p + 1
 				k = k + 1
 				#Concat hidden features along rows for different batches
 				clfr_features = np.append(clfr_features, clfr_encoded_feature_conc, axis=0)
-	
-		predicted_labels = self.mlp_classifier(get_pytorch_variable(clfr_features))
+
+		clfr_features = get_pytorch_variable(clfr_features)	
+		predicted_labels = self.mlp_classifier(clfr_features.cuda())
+		return predicted_labels, decoded_feature_conc
+
+class E2E_NETWORK_TEST(nn.Module):
+	def __init__(self, input_output_dim, hidden_layer_dim, number_of_classes, train_class_labels):
+		super(E2E_NETWORK_TEST, self).__init__()
+		self.input_output_dim = input_output_dim
+		self.hidden_layer_dim = hidden_layer_dim
+		self.number_of_classes = number_of_classes
+		self.train_class_labels = train_class_labels
+		self.input_feature_dim = hidden_layer_dim * number_of_classes * number_of_classes
+		self.mlp_classifier = mlp_classifier(self.input_feature_dim, self.number_of_classes)
+		self.mlp_classifier.cuda()
+		self.ae_bank = []
+		for i in range(self.number_of_classes*self.number_of_classes):
+			ae_model = autoencoder(self.input_output_dim, self.hidden_layer_dim)
+			self.ae_bank.append(ae_model.cuda())
+
+	def forward(self, data):
+		encoded_feature_conc = np.empty((0, self.hidden_layer_dim), dtype=float)		
+		decoded_feature_conc = np.empty((0, self.input_output_dim), dtype=float)
+		
+		#Passing input class data to all cc to form final feature vector
+		clfr_features = np.empty((0, self.hidden_layer_dim*NUMBER_OF_CLASSES*NUMBER_OF_CLASSES), dtype=float)		
+		p = 0
+		clfr_encoded_feature_conc = np.empty((data.shape[0], 0), dtype=float)		
+		for classI in self.train_class_labels:
+			for classJ in self.train_class_labels:
+				clfr_encoded_features, clfr_decoded_features = self.ae_bank[p](data)
+				#Concat hidden features along columns for all cc
+				clfr_encoded_feature_conc = np.append(clfr_encoded_feature_conc, clfr_encoded_features.data.cpu().numpy(), axis=1)
+				p = p + 1
+		
+		#Concat hidden features along rows for different batches/samples
+		clfr_features = np.append(clfr_features, clfr_encoded_feature_conc, axis=0)
+
+		clfr_features = get_pytorch_variable(clfr_features)	
+		predicted_labels = self.mlp_classifier(clfr_features.cuda())
 		return predicted_labels, decoded_feature_conc
 
 '''
@@ -248,19 +333,20 @@ Loss functions
 ---------------------------------------------
 ---------------------------------------------
 '''
-mse_loss_function = nn.MSELoss()
+mse_loss_function = nn.MSELoss(size_average=True)
 cross_entropy_loss_function = nn.CrossEntropyLoss()# the target label is not one-hotted
 
 def get_classifier_loss(predicted_labels, gt_labels):
-	return cross_entropy_loss_function(predicted_labels, gt_labels)
+	#return cross_entropy_loss_function(predicted_labels, gt_labels)
+	return F.nll_loss(predicted_labels, gt_labels)
 
 def get_mse_loss(decoded_data, output_data, train_class_labels):
 	k = 0
 	mse_loss = 0
 	for input_class in train_class_labels:
 		for output_class in train_class_labels:
-			start_ind = k * BATCH_SIZE
-			end_ind = start_ind + BATCH_SIZE				
+			start_ind = k * TRAIN_BATCH_SIZE
+			end_ind = start_ind + TRAIN_BATCH_SIZE				
 			this_cc_output = output_data[start_ind:end_ind, :] 
 			this_cc_decoded_output = decoded_data[start_ind:end_ind, :]
 			k = k + 1
@@ -276,30 +362,74 @@ def train_pytorch_cc(obj_input):
 	train_class_labels = obj_input.train_class_labels
 	#input_indices, output_indices = precompute_training_indices(obj_input.dataset_train_labels, train_class_labels)
 
-	e2e_model_train = E2E_NETWORK_TRAIN(input_output_dim, hidden_layer_dim, number_of_classes, train_class_labels)
-	#e2e_model_train.cuda()
-	optimizer = torch.optim.Adam(e2e_model_train.parameters(), lr=learning_rate, weight_decay=1e-5)
-	for epoch in range(num_epochs):
-		for batch in range(NUMBER_OF_BATCHES):
-			input_data, output_data, gt_labels = get_training_data(batch, obj_input.visual_features, obj_input.dataset_train_labels, train_class_labels)			
+	if 1:	
+		'''
+			----------------------------------------------
+			-------------------Training-------------------
+			----------------------------------------------
+		
+		'''
+		e2e_model_train = E2E_NETWORK_TRAIN(input_output_dim, hidden_layer_dim, number_of_classes, train_class_labels)
+		#e2e_model_train.load_state_dict(torch.load(MODEL_SAVE_PATH))
+		e2e_model_train.train()
+		e2e_model_train.cuda()
+		optimizer = torch.optim.Adam(e2e_model_train.parameters(), lr=learning_rate, weight_decay=1e-5)
+		print e2e_model_train
+		pdb.set_trace()
+		for epoch in range(num_epochs):
+			running_corrects = 0
+			running_total_samples = 0
+			for batch in range(NUMBER_OF_BATCHES):
+				input_data, output_data, gt_labels = get_training_data(batch, obj_input.visual_features, obj_input.dataset_train_labels, train_class_labels)			
+				# ===================forward AE=====================
+				input_data = get_pytorch_variable(input_data)
+				output_data = get_pytorch_variable(output_data)
+				predicted_labels, decoded_feature_conc = e2e_model_train(input_data.cuda())
+				decoded_feature_conc = get_pytorch_variable(decoded_feature_conc)	
+				mse_loss_train = get_mse_loss(decoded_feature_conc.cuda(), output_data.cuda(), train_class_labels)
+				gt_labels = gt_labels - 1
+				classifier_loss_train = get_classifier_loss(predicted_labels, (get_pytorch_long_variable(gt_labels)).cuda())
+			
+				# ===================backward====================
+				total_train_loss = mse_loss_train + alpha * classifier_loss_train
+				optimizer.zero_grad()
+				total_train_loss.backward()
+				optimizer.step()
+			
+				# ===================log========================
+				_, predictions = torch.max(predicted_labels.data, 1)
+				running_corrects += torch.sum(predictions == ((torch.from_numpy(gt_labels)).cuda()))	
+				running_total_samples += predictions.shape[0]
+				train_acc = running_corrects*100.0/running_total_samples
+				print('Batch [%4d/%4d], epoch [%3d/%3d], Total Loss {:%4f}, MSE loss:{:%4f}, Classifier loss:{:%4f} Train Accuracy:{:%4f}'
+				  %(batch, NUMBER_OF_BATCHES, epoch + 1, num_epochs, (mse_loss_train.data[0] + classifier_loss_train.data[0]), mse_loss_train.data[0], classifier_loss_train.data[0], train_acc))
+				torch.save(e2e_model_train.state_dict(), MODEL_SAVE_PATH)
+	
+	else:
+
+		'''
+			----------------------------------------------
+			-------------------Testing-------------------
+			----------------------------------------------
+		'''
+		
+		e2e_model_test = E2E_NETWORK_TEST(input_output_dim, hidden_layer_dim, number_of_classes, train_class_labels)
+		e2e_model_test.load_state_dict(torch.load(MODEL_SAVE_PATH))
+		e2e_model_test.eval()
+		e2e_model_test.cuda()
+		
+		running_corrects = 0
+		running_total_samples = 0
+		for batch in range(NUMBER_OF_TEST_BATCHES):
+			input_data, gt_labels = get_testing_data(batch, obj_input.visual_features, obj_input.dataset_test_labels, train_class_labels)
 			# ===================forward AE=====================
-			predicted_labels, decoded_feature_conc = e2e_model_train(input_data)			
-			mse_loss_train = get_mse_loss(get_pytorch_variable(decoded_feature_conc), get_pytorch_variable(output_data), train_class_labels)
-			predicted_labels = predicted_labels - 1
+			input_data = get_pytorch_variable(input_data)
+			predicted_labels, decoded_feature_conc = e2e_model_test(input_data.cuda())
 			gt_labels = gt_labels - 1
-			classifier_loss_train = get_classifier_loss(predicted_labels, get_pytorch_long_variable(gt_labels))
-
-
-			# ===================backward====================
-			total_train_loss = mse_loss_train + alpha * classifier_loss_train
-			optimizer.zero_grad()
-			total_train_loss.backward()
-			optimizer.step()
 		
 			# ===================log========================
-			print('Batch [%4d/%4d], epoch [%3d/%3d], Total Loss {:%4f}, MSE loss:{:%4f}, Classifier loss:{:%4f}'
-			  %(batch, NUMBER_OF_BATCHES, epoch + 1, num_epochs, (mse_loss_train.data[0] + classifier_loss_train.data[0]), mse_loss_train.data[0], classifier_loss_train.data[0]))
-			torch.save(e2e_model_train.state_dict(), './e2e_model.pth')
-	
-	
-
+			_, predictions = torch.max(predicted_labels.data, 1)
+			running_corrects += torch.sum(predictions.type(torch.FloatTensor) == (torch.from_numpy(gt_labels).type(torch.FloatTensor)))	
+			running_total_samples += predictions.shape[0]
+			test_acc = running_corrects*100.0/running_total_samples
+			print('Batch [%4d/%4d], Test Accuracy:{:%4f}' %(batch, NUMBER_OF_TEST_BATCHES, test_acc))
